9f30fac02b7a0286489110fd1a0fba0fe081f9b6
wamarinj <walteramj@gmail.com>
2022-09-05 20:47:36 -0500

    añadiendo footer y corrigiendo estilos

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+import "./Footer.css"
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <h3>Desing by Walter Marin</h3>
+        </footer>
+    )
+}
+
+export default Footer
